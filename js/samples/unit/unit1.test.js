1186b514643fad80f6778ab601da87e5b6b612d2
gabistam <gabistam@gmail.com>
2022-07-23 14:42:10 +0200

    Unit1.js => test unitaire + test coverage

--- js/samples/unit/unit1.test.js
+++ js/samples/unit/unit1.test.js
@@ -1,7 +1,11 @@
 import { sayHello } from "./unit1";
 
 describe('SayHello Unit Test Suites', () =>{
-    it('Should be true', () => {
-        expect(true).toBe(true)
+    it('Should return "Hello, World"', () => {
+      expect(sayHello()).not.toBe("Hello");
+    });
+
+    it('Should return "Hello, Stam"', () => {
+        expect(sayHello("Stam")).toBe("Hello, Stam")
     })
 })
