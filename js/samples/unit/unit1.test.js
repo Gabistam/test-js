8c4d88b5d457b324a438fcac51ab3fa24e176546
gabistam <gabistam@gmail.com>
2022-07-23 12:39:06 +0200

    test of unit1.js

--- js/samples/unit/unit1.test.js
+++ js/samples/unit/unit1.test.js
@@ -1,18 +1,7 @@
-// Vous importez la fonction à tester
-import { sayHello } from './unit1.js'
+import { sayHello } from "./unit1";
 
-
-// Puis, vous créez le bloc de séries de test (ou Test Suite)
-describe('sayHello Unit Test Suites', () => {
-    it('should display "Hello, World"', () => {
-        expect(sayHello()).toEqual("Hello, World")
-    })
-    
-    it('should display "Bonjour Alexandra"', () => {
-        expect(sayHello("Alexandra")).toEqual("Bonjour Alexandra")
-    })
-    
-    it('should display "Hello, Thomas"', () => {
-        expect(sayHello("Thomas")).toEqual("Hello, Thomas")
+describe('SayHello Unit Test Suites', () =>{
+    it('Should be true', () => {
+        expect(true).toBe(true)
     })
 })
